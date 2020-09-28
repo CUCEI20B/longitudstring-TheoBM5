716337f23b66c00f8a0d0e504c5ac82c946c674e
Teodoro Barba <teodoro.barba4022@alumnos.udg.mx>
2020-09-28 17:12:23 -0500

    prueba

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string cadena;
+    //cin>> cadena;
+    getline(cin, cadena);
+    cout<<cadena.size() <<endl;
 
     return 0;
 }
